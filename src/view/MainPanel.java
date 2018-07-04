00b1a115a3d756fb8308508d09c951ca746c65f5
samsung <samsung@LAPTOP-LG6IAIFF>
2018-07-04 17:58:00 +0900

    맴머vo 수정 회원가입, 로그인 프레임 수정

--- src/view/MainPanel.java
+++ src/view/MainPanel.java
@@ -1,59 +1,53 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import model.vo.Member;
 
 public class MainPanel extends JPanel {
 	private JFrame mf;
 	private JPanel panel;
 	private boolean bLogin = false;
-	private JPanel panel2 = new JPanel();
+	private JPanel loginUp = new JPanel();
+	private JPanel main = new JPanel();
+	private JPanel sub = new JPanel();
 	private MainPanel mp = this;
+	private ArrayList<Member> list = new ArrayList();
 	public MainPanel(JFrame mf){
 		this.mf = mf;
 		panel = this;
+		Font font = new Font("gulim", Font.BOLD, 16);
 
-		//<선 레이아웃>
 		this.setBounds(0, 0, 640, 860);
 		this.setLayout(null);
 		this.setBackground(Color.white);
+		main.setBounds(0, 0, 640, 205);
+		main.setLayout(null);
+		main.setBackground(Color.white);
+		sub.setBounds(0, 205, 640, 860-205);
+		sub.setLayout(null);
+		sub.setBackground(Color.white);
 
-		JButton btn = new JButton("");
-		btn.setLocation(20, 110);
-		btn.setSize(580, 5);
-		JButton btn1 = new JButton("");
-		btn1.setLocation(20, 200);
-		btn1.setSize(580, 5);
-		JButton btn2 = new JButton("");
-		btn2.setLocation(20, 520);
-		btn2.setSize(580, 5); 
+		//뒤로가기 버튼
+		JButton back = new JButton("<");
+		back.setLocation(45, 45);
+		back.setSize(55, 55);  
 
-		//영화, 예매, 스토어 버튼
-		JButton btn3 = new JButton("");
-		btn3.setLocation(40, 130);
-		btn3.setSize(160, 55); 
-		JButton btn4 = new JButton("");
-		btn4.setLocation(40+190, 130);
-		btn4.setSize(160, 55); 
-		JButton btn5 = new JButton("");
-		btn5.setLocation(40+190+190, 130);
-		btn5.setSize(160, 55); 
-
-		//메인화면 포스터
-		JButton btn6 = new JButton("");
-		btn6.setLocation(30, 215);
-		btn6.setSize(560, 295);
-
-		//메인화면 스토어
-		JButton btn7 = new JButton("");
-		btn7.setLocation(30, 535);
-		btn7.setSize(560, 250); 
+		//텍스트칸
+		JLabel menuName = new JLabel("텍스트");
+		menuName.setBackground(Color.white);
+		menuName.setFont(font);
+		menuName.setLocation(120, 45);
+		menuName.setSize(130, 55);
 
 		//로그인
 		JButton login = new JButton("로그인");
@@ -63,7 +57,7 @@ public class MainPanel extends JPanel {
 		login.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				Login l = new Login(mf,panel2,mp);
+				Login l = new Login(mf,loginUp,mp);
 
 			}
 
@@ -76,38 +70,92 @@ public class MainPanel extends JPanel {
 		signUp.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				new SignUp();
+				new SignUp(list);
 
 			}
 
 		});
-		//뒤로가기 버튼
-		JButton btn10 = new JButton("<");
-		btn10.setLocation(45, 45);
-		btn10.setSize(55, 55);  
 
-		//텍스트칸
-		JButton btn11 = new JButton("텍스트");
-		btn11.setLocation(120, 45);
-		btn11.setSize(130, 55);
-		panel2.setBounds(350, 55, 220, 40);
-		panel2.setBackground(Color.white);
-		panel2.setLayout(null);
-		panel2.add(login);
-		panel2.add(signUp);
-		this.add(btn);
-		this.add(btn1);
-		this.add(btn2);
-		this.add(btn3);
-		this.add(btn4);
-		this.add(btn5);
-		this.add(btn6);
-		this.add(btn7);
-		this.add(panel2);
-		//		this.add(login);
-		//		this.add(signUp);
-		this.add(btn10);
-		this.add(btn11);
+		//<선 레이아웃>1
+		JButton line = new JButton("");
+		line.setLocation(20, 110);
+		line.setSize(580, 5);
+
+		//영화, 예매, 스토어 버튼
+		JButton movie = new JButton("영화");
+		movie.setLocation(40, 130);
+		movie.setSize(160, 55);
+		movie.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+
+			}
+
+		});
+		
+		JButton res = new JButton("예매");
+		res.setLocation(40+190, 130);
+		res.setSize(160, 55); 
+		res.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+
+		});
+		
+		JButton store = new JButton("스토어");
+		store.setLocation(40+190+190, 130);
+		store.setSize(160, 55); 
+		store.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+			}
+
+		});
+		
+		//<선 레이아웃>2
+		JButton line1 = new JButton("");
+		line1.setLocation(20, 200);
+		line1.setSize(580, 5);
+		
+		//메인화면 포스터
+		JButton pooster = new JButton("");
+		pooster.setLocation(30, 15);
+		pooster.setSize(560, 295);
+
+		//<선 레이아웃>3
+		JButton line2 = new JButton("");
+		line2.setLocation(20, 295+15+10);
+		line2.setSize(580, 5); 
+
+		//메인화면 스토어
+		JButton storeMenu = new JButton("");
+		storeMenu.setLocation(30, 295+15+10+5+10);
+		storeMenu.setSize(560, 250);
+
+
+
+		loginUp.setBounds(350, 55, 220, 40);
+		loginUp.setBackground(Color.white);
+		loginUp.setLayout(null);
+		loginUp.add(login);
+		loginUp.add(signUp);
+		main.add(line);
+		main.add(line1);
+		main.add(movie);
+		main.add(res);
+		main.add(store);
+		main.add(loginUp);
+		main.add(back);
+		main.add(menuName);
+		sub.add(pooster);
+		sub.add(storeMenu);
+		sub.add(line2);
+		this.add(main);
+		this.add(sub);
 	}
 
 	public void setLogin(boolean b){
